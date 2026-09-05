6aaeb7d051553b946cc0e1311699b3369e0ea09b
Mantii04 <dijamantbehlulaj1@gmail.com>
2026-09-05 21:53:30 +0200

    Remove aggressive read and reboot hooks to fix sleep crash

--- patch_ksu_next.py
+++ patch_ksu_next.py
@@ -32,11 +32,6 @@ patch_file('fs/open.c',
     '\tif (mode & ~S_IRWXO)\n\t\treturn -EINVAL;',
     '\t#ifdef CONFIG_KSU\n\tksu_handle_faccessat(&dfd, &filename, &mode, NULL);\n\t#endif\n\n\tif (mode & ~S_IRWXO)\n\t\treturn -EINVAL;')
 
-# fs/read_write.c - FIXED TARGETING
-patch_file('fs/read_write.c',
-    'SYSCALL_DEFINE3(read, unsigned int, fd, char __user *, buf, size_t, count)\n{\n\tstruct fd f = fdget_pos(fd);\n\tssize_t ret = -EBADF;\n\n\tif (f.file) {',
-    '#ifdef CONFIG_KSU\nextern bool ksu_vfs_read_hook __read_mostly;\nextern __attribute__((cold)) int ksu_handle_sys_read(unsigned int fd,\n\t\t\t\t\t\t       char __user **buf_ptr, size_t *count_ptr);\n#endif\n\nSYSCALL_DEFINE3(read, unsigned int, fd, char __user *, buf, size_t, count)\n{\n\tstruct fd f = fdget_pos(fd);\n\tssize_t ret = -EBADF;\n\n\t#ifdef CONFIG_KSU\n\tif (unlikely(ksu_vfs_read_hook))\n\t\tksu_handle_sys_read(fd, &buf, &count);\n\t#endif\n\n\tif (f.file) {')
-
 # fs/stat.c
 patch_file('fs/stat.c',
     'int vfs_fstatat(int dfd, const char __user *filename, struct kstat *stat,',
@@ -46,11 +41,5 @@ patch_file('fs/stat.c',
     '\tif ((flag & ~(AT_SYMLINK_NOFOLLOW | AT_NO_AUTOMOUNT |',
     '\t#ifdef CONFIG_KSU \n\tksu_handle_stat(&dfd, &filename, &flag);\n\t#endif\n\n\tif ((flag & ~(AT_SYMLINK_NOFOLLOW | AT_NO_AUTOMOUNT |')
 
-# kernel/reboot.c
-patch_file('kernel/reboot.c',
-    'SYSCALL_DEFINE4(reboot, int, magic1, int, magic2, unsigned int, cmd, void __user *, arg)',
-    '#ifdef CONFIG_KSU\nextern int ksu_handle_sys_reboot(int magic1, int magic2, unsigned int cmd, void __user **arg);\n#endif\n\nSYSCALL_DEFINE4(reboot, int, magic1, int, magic2, unsigned int, cmd, void __user *, arg)')
-
-patch_file('kernel/reboot.c',
-    '\tif (!ns_capable(pid_ns->user_ns, CAP_SYS_BOOT))\n\t\treturn -EPERM;',
-    '\t#ifdef CONFIG_KSU\n\tksu_handle_sys_reboot(magic1, magic2, cmd, &arg);\n\t#endif\n\n\tif (!ns_capable(pid_ns->user_ns, CAP_SYS_BOOT))\n\t\treturn -EPERM;')
+# Removed fs/read_write.c and kernel/reboot.c to prevent sleep panics
+print("Skipped read_write.c and reboot.c to prevent sleep crashes.")
